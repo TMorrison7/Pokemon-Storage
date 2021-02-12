85b9bfe569689f1a0a21bd8d0f42827afbc8d32c
Noah Leach <nleach2013@fau.edu>
2021-02-12 15:00:45 -0500

    Basic Log In function added

---
+++ Pokemon/src/main/frontend/signin.js
@@ -0,0 +1,19 @@
+var userName = ""
+var password = ""
+$("#submitButton").click(function () { 
+    userName = ($("input[name='username']").val()) 
+    password = ($("input[name='password']").val())
+    localStorage.setItem("username",userName);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/Login",
+        data: JSON.stringify({"username":userName, "password":password}),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){alert(data);},
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+  });
+    window.location.href = "http://127.0.0.1:5500/Pokemon/src/main/frontend/userPage.html"   
+});
