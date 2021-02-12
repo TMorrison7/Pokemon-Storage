d2d6540c895407251a36c4f1cb20ec0c88fbf457
Noah Leach <nleach2013@fau.edu>
2021-02-12 13:45:41 -0500

    Second Pass at register

--- Pokemon/src/main/frontend/index.js
+++ Pokemon/src/main/frontend/index.js
@@ -3,6 +3,17 @@ var password = ""
 $("#submitButton").click(function () { 
     userName = ($("input[name='username']").val()) 
     localStorage.setItem("username",userName);
-    $.post( "http://localhost:8080/Register", {"UserName":userName, "Password":password} );
+    //$.post( "http://localhost:8080/Register", {"UserName":userName, "Password":password} );
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/Register",
+        data: {"UserName":userName, "Password":password},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){alert(data);},
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+  });
     window.location.href = "http://127.0.0.1:5500/Pokemon/src/main/frontend/userPage.html"   
 });
