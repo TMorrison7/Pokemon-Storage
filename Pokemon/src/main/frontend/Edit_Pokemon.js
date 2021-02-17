add0fcf7f3ca59a54703cbc2d1e506984eaf84c6
Noah Leach <nleach2013@fau.edu>
2021-02-17 11:30:47 -0500

    Added info to Edit_Pokemon

--- Pokemon/src/main/frontend/Edit_Pokemon.js
+++ Pokemon/src/main/frontend/Edit_Pokemon.js
@@ -1,7 +1,39 @@
 var pokeID = localStorage.getItem("pokeID");
 
+$.getJSON("http://localhost:8080/store/pokemonNames/id/" + pokeID,
+    function (data) {
+        $("#name").val(data['pokemonName']);
+    }
+);
+
+$.getJSON("http://localhost:8080/store/pokemon/byType/" + pokeID,
+    function (data) {
+        let types = "";
+        $.each(data, function (indexInArray, valueOfElement) { 
+            if (indexInArray == 1){
+                types += '/';
+            }
+            types += valueOfElement['typeName'];
+        });
+        $("#Type").val(types);
+    }
+);
+
+$.getJSON("http://localhost:8080/store/pokemon/byAbility/" + pokeID,
+    function (data) {
+        $("#Ability").val(data['ability']);
+    }
+);
+
 $.getJSON("http://localhost:8080/store/pokemon/"+pokeID,
     function (data) {
-        $("#name").val(data.name);
+        $("#Level").val(data['level']);
+        $("#HP").val(data['hp']);
+        $("#Attack").val(data['attack']);
+        $("#Defense").val(data['defense']);
+        $("#Speed").val(data['speed']);
+        $("#Special_Attack").val(data['spAttack']);
+        $("#Special_Defense").val(data['spDefense']);
+        
     }
 );
