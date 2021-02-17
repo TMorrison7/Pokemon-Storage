177c9cf963436be095c205ad8c524c80f30eec5c
Noah Leach <nleach2013@fau.edu>
2021-02-17 14:20:59 -0500

    Edit Pokemon cross origin error

--- Pokemon/src/main/frontend/Edit_Pokemon.js
+++ Pokemon/src/main/frontend/Edit_Pokemon.js
@@ -1,4 +1,6 @@
 var pokeID = localStorage.getItem("pokeID");
+var userID = 0;
+var uniqueID = 0;
 
 $.getJSON("http://localhost:8080/store/pokemonNames/id/" + pokeID,
     function (data) {
@@ -27,6 +29,8 @@ $.getJSON("http://localhost:8080/store/pokemon/byAbility/" + pokeID,
 
 $.getJSON("http://localhost:8080/store/pokemon/"+pokeID,
     function (data) {
+        userID = data['userID'];
+        uniqueID = data['uniqueID'];
         $("#Level").val(data['level']);
         $("#HP").val(data['hp']);
         $("#Attack").val(data['attack']);
@@ -44,3 +48,66 @@ $.getJSON("http://localhost:8080/store/pokemon/"+pokeID,
         );
     }
 );
+
+$("#SaveChanges").click(function () { 
+    let abilityID = 0;
+    $.getJSON("http://localhost:8080/store/abilities/" + $("#Ability").val(),
+        function (data3) {
+            abilityID = data3['abilityID']
+        }
+    ); 
+
+    let moveID_1 = 0;
+    let moveID_2 = 0;
+    let moveID_3 = 0;
+    let moveID_4 = 0;
+    $.getJSON("http://localhost:8080/store/moves/" + $("#Move1").val(),
+        function (temp1) {
+            moveID_1 = temp1['move_ID'];
+        }
+    ); 
+    $.getJSON("http://localhost:8080/store/moves/" + $("#Move2").val(),
+        function (temp2) {
+            moveID_2 = temp2['move_ID'];
+        }
+    );
+    $.getJSON("http://localhost:8080/store/moves/" + $("#Move3").val(),
+        function (temp3) {
+            moveID_3 = temp3['move_ID'];
+        }
+    );
+    $.getJSON("http://localhost:8080/store/moves/" + $("#Move4").val(),
+        function (temp4) {
+            moveID_4 = temp4['move_ID'];
+        }
+    );
+
+    let data = {
+        "pokeID": pokeID,
+        "userID": userID,
+        "level": $("#Level").val(),
+        "abilityID": abilityID,
+        "moveID_1": moveID_1,
+        "moveID_2": moveID_2,
+        "moveID_3": moveID_3,
+        "moveID_4": moveID_4,
+        "attack": $("#Attack").val(),
+        "defense": $("#Defense").val(),
+        "speed": $("#Speed").val(),
+        "spAttack": $("#Special_Attack").val(),
+        "spDefense": $("#Special_Defense").val(),
+        "uniqueID": uniqueID,
+        "hp": $("#HP").val()
+    }
+
+    $.ajax({
+        type: 'PUT',
+        url: 'http://localhost:8080/store/pokemon/alter',
+        contentType: 'json',
+        data: JSON.stringify(data), 
+    }).done(function () {
+        console.log('SUCCESS');
+    }).fail(function (msg) {
+        console.log('FAIL');
+    });
+});
