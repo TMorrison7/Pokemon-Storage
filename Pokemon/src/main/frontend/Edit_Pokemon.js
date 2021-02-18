933b95af82432ab2135136534d3c05a60986fa59
Daniel Rigolini <drigolin@uncc.edu>
2021-02-18 10:40:03 -0500

    Finish Project

--- Pokemon/src/main/frontend/Edit_Pokemon.js
+++ Pokemon/src/main/frontend/Edit_Pokemon.js
@@ -1,5 +1,4 @@
 var pokeID = localStorage.getItem("pokeID");
-<<<<<<< HEAD
 var ability = localStorage.getItem("ability");
 var unique = localStorage.getItem("unique");
 var userID;
@@ -9,10 +8,6 @@ var moveID_1;
 var moveID_2;
 var moveID_3;
 var moveID_4;
-=======
-var userID = 0;
-var uniqueID = 0;
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
 
 $.getJSON("http://localhost:8080/store/pokemonNames/id/" + pokeID,
     function (data) {
@@ -23,11 +18,7 @@ $.getJSON("http://localhost:8080/store/pokemonNames/id/" + pokeID,
 $.getJSON("http://localhost:8080/store/pokemon/byType/" + pokeID,
     function (data) {
         let types = "";
-<<<<<<< HEAD
         $.each(data, function (indexInArray, valueOfElement) {
-=======
-        $.each(data, function (indexInArray, valueOfElement) { 
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
             if (indexInArray == 1){
                 types += '/';
             }
@@ -37,21 +28,13 @@ $.getJSON("http://localhost:8080/store/pokemon/byType/" + pokeID,
     }
 );
 
-<<<<<<< HEAD
 $.getJSON("http://localhost:8080/store/pokemon/byAbility/" + ability,
-=======
-$.getJSON("http://localhost:8080/store/pokemon/byAbility/" + pokeID,
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
     function (data) {
         $("#Ability").val(data['ability']);
     }
 );
 
-<<<<<<< HEAD
 $.getJSON("http://localhost:8080/store/pokemon/"+unique,
-=======
-$.getJSON("http://localhost:8080/store/pokemon/"+pokeID,
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
     function (data) {
         userID = data['userID'];
         uniqueID = data['uniqueID'];
@@ -62,17 +45,10 @@ $.getJSON("http://localhost:8080/store/pokemon/"+pokeID,
         $("#Speed").val(data['speed']);
         $("#Special_Attack").val(data['spAttack']);
         $("#Special_Defense").val(data['spDefense']);
-<<<<<<< HEAD
 
         $.getJSON("http://localhost:8080/store/pokemon/byMoves/" + data['uniqueID'],
             function (data2) {
                 $.each(data2, function (indexInArray, valueOfElement) {
-=======
-        
-        $.getJSON("http://localhost:8080/store/pokemon/byMoves/" + data['uniqueID'],
-            function (data2) {
-                $.each(data2, function (indexInArray, valueOfElement) { 
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
                     $("#Move" + (indexInArray+1)).val(data2[indexInArray]['moveName']);
                 });
             }
@@ -80,7 +56,6 @@ $.getJSON("http://localhost:8080/store/pokemon/"+pokeID,
     }
 );
 
-<<<<<<< HEAD
 
 function getPokemonAbilityIDByName(ability) {
 $.ajax({
@@ -210,50 +185,6 @@ $("#SaveChanges").click(function () {
         "moveID_2": getMoveName2($("#Move2").val()),
         "moveID_3": getMoveName3($("#Move3").val()),
         "moveID_4": getMoveName4($("#Move4").val()),
-=======
-$("#SaveChanges").click(function () { 
-    let abilityID = 0;
-    $.getJSON("http://localhost:8080/store/abilities/" + $("#Ability").val(),
-        function (data3) {
-            abilityID = data3['abilityID']
-        }
-    ); 
-
-    let moveID_1 = 0;
-    let moveID_2 = 0;
-    let moveID_3 = 0;
-    let moveID_4 = 0;
-    $.getJSON("http://localhost:8080/store/moves/" + $("#Move1").val(),
-        function (temp1) {
-            moveID_1 = temp1['move_ID'];
-        }
-    ); 
-    $.getJSON("http://localhost:8080/store/moves/" + $("#Move2").val(),
-        function (temp2) {
-            moveID_2 = temp2['move_ID'];
-        }
-    );
-    $.getJSON("http://localhost:8080/store/moves/" + $("#Move3").val(),
-        function (temp3) {
-            moveID_3 = temp3['move_ID'];
-        }
-    );
-    $.getJSON("http://localhost:8080/store/moves/" + $("#Move4").val(),
-        function (temp4) {
-            moveID_4 = temp4['move_ID'];
-        }
-    );
-
-    let data = {
-        "pokeID": pokeID,
-        "userID": userID,
-        "level": $("#Level").val(),
-        "abilityID": abilityID,
-        "moveID_1": moveID_1,
-        "moveID_2": moveID_2,
-        "moveID_3": moveID_3,
-        "moveID_4": moveID_4,
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
         "attack": $("#Attack").val(),
         "defense": $("#Defense").val(),
         "speed": $("#Speed").val(),
@@ -262,7 +193,6 @@ $("#SaveChanges").click(function () {
         "uniqueID": uniqueID,
         "hp": $("#HP").val()
     }
-<<<<<<< HEAD
 if($("#Level").val() == "" || $("#Level").val() <= 0 || $("#Level").val() > 100) {
   alert("Please enter a valid number for Level");
 }
@@ -301,26 +231,13 @@ else if ($('#Move1').val() == "N/A" && $('#Move2').val() == "N/A" && $('#Move3')
         data: JSON.stringify(data),
     }).done(function () {
         window.location.replace("userPage.html");
-=======
-
-    $.ajax({
-        type: 'PUT',
-        url: 'http://localhost:8080/store/pokemon/alter',
-        contentType: 'json',
-        data: JSON.stringify(data), 
-    }).done(function () {
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
         console.log('SUCCESS');
     }).fail(function (msg) {
         console.log('FAIL');
     });
-<<<<<<< HEAD
   }
 });
 
 $("#Leave").click(function () {
     window.location.replace("userPage.html");
 })
-=======
-});
->>>>>>> 177c9cf963436be095c205ad8c524c80f30eec5c
